fcbec73ead2501fb123e57d848bf4eba4a5e32b2
JagTheFriend <jagthefriend12@gmail.com>
2021-11-25 17:32:21 +0200

    Updated interface

--- src/interfaces/article.interface.ts
+++ src/interfaces/article.interface.ts
@@ -1,13 +1,14 @@
+import { Document } from 'mongoose';
+
 /**
  * @interface Article
  * @description Stores the structure of a article
  */
-export interface Article {
+export interface Article extends Document {
   title: string;
   description: string;
   markdown: string;
   createdAt: Date;
   slug: string;
   sanitizedHtml: string;
-  save?: Function;
 }
