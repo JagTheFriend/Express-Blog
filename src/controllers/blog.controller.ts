15e77f8117243319b550aced30a408fb0a205b64
JagTheFriend <jagthefriend12@gmail.com>
2021-11-25 14:57:41 +0200

    Added the `/new` and the `/:slug` route

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,12 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 
+// temporary
+import { Model, Document } from 'mongoose';
+declare const Article: Model<Document>;
+
 class BlogController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.send('Blog Route!');
+      res.render('articles/new');
     } catch (error) {
       next(error);
     }
+  };
+
+  public slug = async (req: Request, res: Response) => {
+    const article = await Article.findOne({ slug: req.params.slug });
+    // check whether the article exists
+    if (article == null) return res.redirect('/');
+    res.render('articles/show', { article: article });
   };
 }
 
