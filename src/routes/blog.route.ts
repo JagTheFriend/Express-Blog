11860f170862af1a26f50e6de8b386a749ebfad2
JagTheFriend <jagthefriend12@gmail.com>
2021-11-26 12:09:57 +0200

    Fixed bug

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import ArticleController from '@controllers/article.controller';
 import { Routes } from '@interfaces/routes.interface';
+import { ArticleDto } from '@dtos/article.dto';
 import authMiddleware from '@middlewares/auth.middleware';
+import validationMiddleware from '@middlewares/validation.middleware';
 
 class ArticleRoute implements Routes {
   public path = '/articles';
@@ -13,15 +15,15 @@ class ArticleRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.articleController.index);
+    this.router.get(`${this.path}`, authMiddleware, this.articleController.newArticle);
+    // this.router.get(`${this.path}/new`, this.articleController.newArticle);
+
     this.router.get(`${this.path}/:slug`, this.articleController.slug);
+    this.router.get(`${this.path}/edit/:id`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.editArticle);
 
-    this.router.get(`${this.path}/new`, authMiddleware, this.articleController.index);
-    this.router.get(`${this.path}/edit/:id`, authMiddleware, this.articleController.editArticle);
-
-    this.router.put(`${this.path}/save/:id`, authMiddleware, this.articleController.saveArticle);
-    this.router.post(`${this.path}/`, authMiddleware, this.articleController.newArticle);
-    this.router.delete(`${this.path}/:id`, authMiddleware, this.articleController.deleteArticle);
+    this.router.put(`${this.path}/save/:id`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.saveArticle);
+    this.router.post(`${this.path}/`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.newArticle);
+    this.router.delete(`${this.path}/:id`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.deleteArticle);
   }
 }
 
