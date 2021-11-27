ad48d6d85bfdffa1b57e3f45a901d2edc0053a90
JagTheFriend <jagthefriend12@gmail.com>
2021-11-27 12:07:34 +0200

    Updated route

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -25,8 +25,8 @@ class ArticleRoute implements Routes {
     this.router.delete(`${this.path}/:id`, authMiddleware, this.articleController.deleteArticle);
     this.router.get(`${this.path}/edit/:id`, authMiddleware, this.articleController.editArticle);
 
-    this.router.put(`${this.path}/save/:id`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.saveArticle);
-    this.router.post(`${this.path}/`, authMiddleware, validationMiddleware(ArticleDto, 'body', false), this.articleController.createNewArticle);
+    this.router.put(`${this.path}/save/:id`, authMiddleware, validationMiddleware(ArticleDto, 'body'), this.articleController.saveArticle);
+    this.router.post(`${this.path}/`, authMiddleware, validationMiddleware(ArticleDto, 'body'), this.articleController.createNewArticle);
   }
 }
 
